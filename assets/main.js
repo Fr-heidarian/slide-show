06a8721af74f8e7cc2658f8c071d23f004750122
Farideh <heidarian89@gmail.com>
2023-11-26 00:03:55 -0800

    dynamic indicators

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,31 @@
 let index = 0;
 const images = document.querySelectorAll(".images-box img");
+const indicators = document.querySelectorAll(".indicators span");
+
+const updateIndicators = () => {
+  for (let i = 0; i < indicators.length; i++) {
+    if (i === index) {
+      // indicators[i].style.background = "red";
+      indicators[i].classList.add("active");
+    } else {
+      // indicators[i].style.background = "";
+      indicators[i].classList.remove("active");
+    }
+  }
+};
+// updateIndicators();
 
 const nextSlide = () => {
-  () => {
-    images[index].style.display = "none";
-    index++; 
-    if (index >= images.length) {
-      index = 0;
-    }
-    images[index].style.display = "block";
-  };
+  images[index].style.display = "none";
+
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+  images[index].style.display = "block";
+  updateIndicators();
 };
+
 const previousSlide = () => {
   images[index].style.display = "none";
   index--;
@@ -18,6 +33,7 @@ const previousSlide = () => {
     index = images.length - 1;
   }
   images[index].style.display = "block";
+  updateIndicators();
 };
 
 document.querySelector(".next").addEventListener("click", nextSlide);
